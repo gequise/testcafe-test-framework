e9289382519c40c933d6c1d379d101ae8af29991
gequise <gustavo.equise@gmail.com>
2026-01-02 16:24:38 -0300

    update: test file

--- tests/testCafe.ts
+++ tests/testCafe.ts
@@ -6,9 +6,9 @@ fixture `Parabank Login Test`
 // Tests
 test('Text typing basics', async t => {
     await t
-        .typeText(loginPage.usernameInput, 'gustavo')
+        .typeText(loginPage.usernameInput, 'Gustavo')
         .typeText(loginPage.passwordInput, 'ABC123')
-        .expect(loginPage.usernameInput.value).eql('gustavo')
+        .expect(loginPage.usernameInput.value).eql('Gustavo')
         .click(loginPage.loginButton)
         // .expect(loginPage.welcomeText.innerText).eql('Welcome gustavo gustavo', 'The Welcome text is correct');
 });
